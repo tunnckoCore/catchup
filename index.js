df0a1230a5fc09e9ee538fac57290268ca788753
tunnckoCore <mameto_100@mail.bg>
2015-09-11 00:27:34 +0300

    implement :cat2:

---
+++ index.js
@@ -0,0 +1,50 @@
+/*!
+ * catchup <https://github.com/tunnckoCore/catchup>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+var Dual = require('dual-emitter')
+
+function Catchup () {
+  if (!(this instanceof Catchup)) {
+    return new Catchup()
+  }
+  Dual.call(this)
+
+  process.once('newListener', function (name) {
+    this.removeAllListeners(name)
+  })
+}
+Dual.extend(Catchup)
+
+Catchup.create = function () {
+  return new Catchup()
+}
+
+Catchup.prototype.run = function (fn) {
+  var ret = null
+
+  try {
+    ret = fn.apply(this, arguments)
+  } catch (err) {
+    return this.emit('error', err)
+  }
+
+  process.once('uncaughtException', function onError (err) {
+    this.emit('error', err)
+  }.bind(this))
+
+  return ret
+}
+
+/**
+ * Expose `Catchtup`
+ */
+
+module.exports = Catchup
+module.exports.Catchup = Catchup
+
---
+++ test.js
@@ -0,0 +1,49 @@
+/*!
+ * catchup <https://github.com/tunnckoCore/catchup>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+var fs = require('fs')
+var test = require('assertit')
+var catchup = require('./index')
+var Catchup = require('./index').Catchup
+
+test('should handle basic errors', function (done) {
+  var domain = new Catchup()
+  domain.once('error', function (err) {
+    test.ifError(!err)
+    test.ok(err instanceof Error)
+    test.equal(err.message, 'foo bar')
+    done()
+  })
+  domain.run(function () {
+    throw new Error('foo bar')
+  })
+})
+
+test('should handle ENOENT errors', function (done) {
+  var domain = catchup()
+  domain.once('error', function (err) {
+    test.ok(err instanceof Error)
+    test.equal(err.code, 'ENOENT')
+    done()
+  })
+  domain.run(function () {
+    fs.createReadStream('foo bar')
+  })
+})
+
+test('should have static `.create` method', function (done) {
+  var domain = Catchup.create()
+  var res = domain.run(function () {
+    return 'foo bar baz qux'
+  })
+  test.equal(res, 'foo bar baz qux')
+  done()
+})
